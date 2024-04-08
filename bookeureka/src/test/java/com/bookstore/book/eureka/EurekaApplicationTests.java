88b9e9255d8d3f4733666dbb3bca478c2be031ce
nagasani <nagasani.r@gmail.com>
2024-04-07 22:13:06 -0400

    Initial

---
+++ bookeureka/src/test/java/com/bookstore/book/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.book.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
